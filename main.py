3528eed518f106f18dfa96a3baa4e1ad1ee8ddc8
Muneeb Arshad <42831583+mnbrshd@users.noreply.github.com>
2024-02-08 14:44:15 +0500

    created an API

---
+++ main.py
@@ -0,0 +1,35 @@
+import uvicorn
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI
+from mylib.bot import scrape
+
+app = FastAPI()
+
+
+class Wiki(BaseModel):
+    name: str
+
+
+@app.post('/')
+async def predict_story(wiki: Wiki):
+    result = scrape(wiki.name)
+    payload = {"wikipage": result}
+    json_compatible_item_data = jsonable_encoder(payload)
+    return JSONResponse(content=json_compatible_item_data)
+
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello Model'}
+
+
+@app.get("/add/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    """Add two numbers together"""
+
+    total = num1 + num2
+    return {"total": total}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host='0.0.0.0')
