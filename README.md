fc87fc5e641ca7cd217bb637136a49c68775a8a2
Muneeb Arshad <42831583+mnbrshd@users.noreply.github.com>
2024-02-11 06:04:25 +0000

    added dockerfile

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 live training
 
 [![Python application test with Github Actions](https://github.com/mnbrshd/functions-from-zero/actions/workflows/main.yml/badge.svg)](https://github.com/mnbrshd/functions-from-zero/actions/workflows/main.yml)
+
+
+### To call Microservices
+
+something like this
+``` bash
+curl -X 'POST' \
+  'https://reimagined-carnival-jq59gw6v4pg2qr69-8000.app.github.dev/wiki' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "name": "Microsoft"
+}'
+```
+
+### Build container
+`docker build .`
+`docker image ls`
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi import FastAPI
+from pydantic import BaseModel
 from mylib.bot import scrape
 
 app = FastAPI()
@@ -11,8 +12,8 @@ class Wiki(BaseModel):
     name: str
 
 
-@app.post('/')
-async def predict_story(wiki: Wiki):
+@app.post('/wiki')
+async def scrape_story(wiki: Wiki):
     result = scrape(wiki.name)
     payload = {"wikipage": result}
     json_compatible_item_data = jsonable_encoder(payload)
@@ -21,7 +22,7 @@ async def predict_story(wiki: Wiki):
 
 @app.get('/')
 async def root():
-    return {'message': 'Hello Model'}
+    return {'message': 'Hello Functions'}
 
 
 @app.get("/add/{num1}/{num2}")
